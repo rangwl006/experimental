2eff0897af71074b971b37f708398debe4f6dd43
Russell Ang <rangwl006@gmail.com>
2023-03-14 08:21:17 +0800

    added curl wrapper

--- 00_Dump/test/ConcurrencyTest.cpp
+++ 00_Dump/test/ConcurrencyTest.cpp
@@ -5,15 +5,58 @@
 #include <thread>
 #include <iostream>
 #include <future>
+#include "curl/curl.h"
+#include <memory>
+#include <fstream>
 
-//int execute_async()
-//{
-//    return 5;
-//}
+/*
+ * This exercise wraps around the curl handler
+ */
+
+class CurlWrapper
+{
+public:
+    CurlWrapper():m_curlHandle(curl_easy_init(), m_curlPtrDeleter) { };
+    ~CurlWrapper() = default; // also leave as default for now
+
+    struct CurlPtrDeleter
+    {
+        void operator()(CURL* handle)
+        {
+            std::cout << "CurlPtrDeleter" << std::endl;
+            curl_easy_cleanup(handle);
+        };
+    } m_curlPtrDeleter;
+
+    void PullContent(std::string link)
+    {
+        std::string readBuffer;
+
+        curl_easy_setopt(m_curlHandle.get(), CURLOPT_URL, link.c_str());
+        curl_easy_setopt(m_curlHandle.get(), CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(m_curlHandle.get(), CURLOPT_WRITEDATA, &readBuffer);
+        CURLcode res = curl_easy_perform(m_curlHandle.get());
+
+        std::ofstream out("/builder/doc.html" );
+        out << readBuffer;
+//        std::cout << readBuffer << std::endl;
+    }
+private:
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+    {
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
+        return size * nmemb;
+    };
+    std::unique_ptr<CURL, decltype(m_curlPtrDeleter)> m_curlHandle;
+};
 
 int main()
 {
-    auto x = std::async(std::launch::async, [](){ return 5; });
-    std::cout << x.get() << std::endl;
+    CurlWrapper c;
+//    c.PullContent("www.aldo.com");
+    c.PullContent("https://www.sunteccity.com.sg");
+//    c.PullContent("www.astons.com.sg");
+//    c.PullContent("www.allkpop.com");
+
     return 0;
 };
